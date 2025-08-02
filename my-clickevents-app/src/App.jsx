adc4655c47275fb4b55973fa2e8544888372e4ca
Babayaga-cell <almirol.lexter@gmail.com>
2025-08-02 19:28:58 +0800

    click events.

---
+++ my-clickevents-app/src/App.jsx
@@ -0,0 +1,16 @@
+// click event = An interaction when a user clicks on a
+// specific element, we can respond to clicks by passing
+// a callbacks to the onClick event handler
+
+import Button from "./Button.jsx";
+import ProfilePicture from "./ProfilePicture.jsx";
+
+function App() {
+  return (
+    <>
+      <ProfilePicture />
+    </>
+  );
+}
+
+export default App;
---
+++ my-clickevents-app/src/Button.jsx
@@ -0,0 +1,25 @@
+function Button() {
+  //   const handleClick = () => console.log("ouch");
+  //   const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+  //   return <button onClick={handleClick()}>Click me 😊</button>;
+  //   return <button onClick={() => handleClick2("Bro")}>Click me 😊</button>;
+
+  //   let count = 0;
+  //   const handleClick = (name) => {
+  //     if (count < 3) {
+  //       count++;
+  //       console.log(`${name} you clicked me ${count} time/s`);
+  //     } else {
+  //       console.log(`${name} stop clicking me!`);
+  //     }
+  //   };
+  //   return <button onClick={() => handleClick("Bro")}>Click me 😊</button>;
+
+  //   const handleClick = (e) => (e.target.textContent = "OUCH!");
+  //   return <button onClick={(e) => handleClick(e)}>Click me 😊</button>;
+  // return <button onDoubleClick={(e) => handleClick(e)}>Click me 😊</button>;
+
+  
+}
+
+export default Button;
